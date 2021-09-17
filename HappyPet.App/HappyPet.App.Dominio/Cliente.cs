68b8727200281bcb6055a064ef837ec43020e9f8
Veronica Aguirre <veronicagomez2912@gmail.com>
2021-09-17 15:34:03 -0500

    Creacion de entidad <Cliente>

---
+++ HappyPet.App/HappyPet.App.Dominio/Cliente.cs
@@ -0,0 +1,9 @@
+using System;
+namespace HospiEnCasa.App.Dominio
+{
+    public class Cliente : Persona
+    {
+        private int Id {set; get;}
+        private string Direccion {set; get;}
+    }
+}
