8cdd7a2704abbe86df7425d10d766b7336a98810
William Beltran <willing27@gmail.com>
2021-09-17 16:11:02 -0500

    Creacion entidades <HistoriaClinica y Visita>

---
+++ HappyPet.App/HappyPet.App.Dominio/HistoriaClinica.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HappyPet.App.Dominio
+{
+    public class HistoriaClinica{
+        private int ID {get;set;}
+        private double Peso {get;set;}
+        private double temperatura {get;set;}
+        private double FrecuenciaRespiratoria {get;set;}
+        private double FrecuenciaCardiaca {get;set;}
+        private string EstadoDeSalud {get;set;}
+    }
+}
